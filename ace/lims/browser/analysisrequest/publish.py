12171dc8c054a8651e522fcb77b90bca8c9a7675
Lunga Baliwe <lunga001@gmail.com>
2017-06-10 10:19:01 +0200

    Copied BC-14 Code(CSV results)

--- ace/lims/browser/analysisrequest/publish.py
+++ ace/lims/browser/analysisrequest/publish.py
@@ -1,16 +1,31 @@
 from DateTime import DateTime
+from Products.CMFCore.WorkflowCore import WorkflowException
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode, _createObjectByType
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from ace.lims.vocabularies import  getACEARReportTemplates
 from bika.lims.browser.analysisrequest.publish import \
     AnalysisRequestPublishView as ARPV
-from ace.lims.vocabularies import  getACEARReportTemplates
+from bika.lims.idserver import renameAfterCreation
 from bika.lims import bikaMessageFactory as _, t
-from bika.lims.utils import to_utf8, getUsers
+from bika.lims import logger
+from bika.lims.utils import to_utf8, encode_header, createPdf, attachPdf, \
+        attachCSV
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.Utils import formataddr
+from smtplib import SMTPServerDisconnected, SMTPRecipientsRefused
 from plone.app.content.browser.interfaces import IFolderContentsView
+from plone.resource.utils import  queryResourceDirectory
 from zope.interface import implements
 
+import App
+import StringIO
+import csv
 import os, traceback
+import tempfile
+import time
 
 class AnalysisRequestPublishView(ARPV):
     implements(IFolderContentsView)
@@ -314,3 +329,198 @@ class AnalysisRequestPublishView(ARPV):
                 if unit not in analyses[cat]['_data']['unit']:
                     analyses[cat]['_data']['unit'].append(unit)
         return analyses
+
+    def publishFromHTML(self, aruid, results_html):
+        # The AR can be published only and only if allowed
+        uc = getToolByName(self.context, 'uid_catalog')
+        ars = uc(UID=aruid)
+        if not ars or len(ars) != 1:
+            return []
+
+        ar = ars[0].getObject();
+        wf = getToolByName(ar, 'portal_workflow')
+        allowed_states = ['verified', 'published']
+        # Publish/Republish allowed?
+        if wf.getInfoFor(ar, 'review_state') not in allowed_states:
+            # Pre-publish allowed?
+            if not ar.getAnalyses(review_state=allowed_states):
+                return []
+
+        # HTML written to debug file
+        debug_mode = App.config.getConfiguration().debug_mode
+        if debug_mode:
+            tmp_fn = tempfile.mktemp(suffix=".html")
+            logger.debug("Writing HTML for %s to %s" % (ar.Title(), tmp_fn))
+            open(tmp_fn, "wb").write(results_html)
+
+        # Create the pdf report (will always be attached to the AR)
+        # we must supply the file ourself so that createPdf leaves it alone.
+        pdf_fn = tempfile.mktemp(suffix=".pdf")
+        pdf_report = createPdf(htmlreport=results_html, outfile=pdf_fn)
+
+        # PDF written to debug file
+        if debug_mode:
+            logger.debug("Writing PDF for %s to %s" % (ar.Title(), pdf_fn))
+        else:
+            os.remove(pdf_fn)
+
+        recipients = []
+        contact = ar.getContact()
+        lab = ar.bika_setup.laboratory
+
+        # BIKA Cannabis hack.  Create the CSV they desire here now
+        csvdata = self.create_cannabis_csv(ars)
+        if pdf_report:
+            if contact:
+                recipients = [{
+                    'UID': contact.UID(),
+                    'Username': to_utf8(contact.getUsername()),
+                    'Fullname': to_utf8(contact.getFullname()),
+                    'EmailAddress': to_utf8(contact.getEmailAddress()),
+                    'PublicationModes': contact.getPublicationPreference()
+                }]
+            reportid = ar.generateUniqueId('ARReport')
+            report = _createObjectByType("ARReport", ar, reportid)
+            report.edit(
+                AnalysisRequest=ar.UID(),
+                Pdf=pdf_report,
+                Html=results_html,
+                Recipients=recipients
+            )
+            report.unmarkCreationFlag()
+            renameAfterCreation(report)
+
+            # Set status to prepublished/published/republished
+            status = wf.getInfoFor(ar, 'review_state')
+            transitions = {'verified': 'publish',
+                           'published' : 'republish'}
+            transition = transitions.get(status, 'prepublish')
+            try:
+                wf.doActionFor(ar, transition)
+            except WorkflowException:
+                pass
+
+            # compose and send email.
+            # The managers of the departments for which the current AR has
+            # at least one AS must receive always the pdf report by email.
+            # https://github.com/bikalabs/Bika-LIMS/issues/1028
+            mime_msg = MIMEMultipart('related')
+            mime_msg['Subject'] = self.get_mail_subject(ar)[0]
+            mime_msg['From'] = formataddr(
+                (encode_header(lab.getName()), lab.getEmailAddress()))
+            mime_msg.preamble = 'This is a multi-part MIME message.'
+            msg_txt = MIMEText(results_html, _subtype='html')
+            mime_msg.attach(msg_txt)
+
+            to = []
+            mngrs = ar.getResponsible()
+            for mngrid in mngrs['ids']:
+                name = mngrs['dict'][mngrid].get('name', '')
+                email = mngrs['dict'][mngrid].get('email', '')
+                if (email != ''):
+                    to.append(formataddr((encode_header(name), email)))
+
+            if len(to) > 0:
+                # Send the email to the managers
+                mime_msg['To'] = ','.join(to)
+                attachPdf(mime_msg, pdf_report, ar.id)
+
+                # BIKA Cannabis hack.  Create the CSV they desire here now
+                fn = self.current_certificate_number()
+                attachCSV(mime_msg,csvdata,fn)
+
+                try:
+                    host = getToolByName(ar, 'MailHost')
+                    host.send(mime_msg.as_string(), immediate=True)
+                except SMTPServerDisconnected as msg:
+                    logger.warn("SMTPServerDisconnected: %s." % msg)
+                except SMTPRecipientsRefused as msg:
+                    raise WorkflowException(str(msg))
+
+        # Send report to recipients
+        recips = self.get_recipients(ar)
+        for recip in recips:
+            if 'email' not in recip.get('pubpref', []) \
+                    or not recip.get('email', ''):
+                continue
+
+            title = encode_header(recip.get('title', ''))
+            email = recip.get('email')
+            formatted = formataddr((title, email))
+
+            # Create the new mime_msg object, cause the previous one
+            # has the pdf already attached
+            mime_msg = MIMEMultipart('related')
+            mime_msg['Subject'] = self.get_mail_subject(ar)[0]
+            mime_msg['From'] = formataddr(
+            (encode_header(lab.getName()), lab.getEmailAddress()))
+            mime_msg.preamble = 'This is a multi-part MIME message.'
+            msg_txt = MIMEText(results_html, _subtype='html')
+            mime_msg.attach(msg_txt)
+            mime_msg['To'] = formatted
+
+            # Attach the pdf to the email if requested
+            if pdf_report and 'pdf' in recip.get('pubpref'):
+                attachPdf(mime_msg, pdf_report, ar.id)
+                # BIKA Cannabis hack.  Create the CSV they desire here now
+                fn = self.current_certificate_number()
+                attachCSV(mime_msg,csvdata,fn)
+
+            # For now, I will simply ignore mail send under test.
+            if hasattr(self.portal, 'robotframework'):
+                continue
+
+            msg_string = mime_msg.as_string()
+
+            # content of outgoing email written to debug file
+            if debug_mode:
+                tmp_fn = tempfile.mktemp(suffix=".email")
+                logger.debug("Writing MIME message for %s to %s" % (ar.Title(), tmp_fn))
+                open(tmp_fn, "wb").write(msg_string)
+
+            try:
+                host = getToolByName(ar, 'MailHost')
+                host.send(msg_string, immediate=True)
+            except SMTPServerDisconnected as msg:
+                logger.warn("SMTPServerDisconnected: %s." % msg)
+            except SMTPRecipientsRefused as msg:
+                raise WorkflowException(str(msg))
+
+        return [ar]
+
+    def create_cannabis_csv(self, ars):
+        analyses = []
+        output = StringIO.StringIO()
+        for ar in ars:
+            sample = ar.getSample()
+            date_rec = ar.getDateReceived()
+            if date_rec:
+                date_rec = date_rec.strftime('%m-%d-%y')
+            sampling_date = ar.getSamplingDate()
+            if sampling_date:
+                sampling_date = sampling_date.strftime('%m-%d-%y')
+            writer = csv.writer(output)
+            writer.writerow(["Sample Type", sample.getSampleType().Title()])
+            writer.writerow(["Client's Ref", ar.getClientReference()])
+            writer.writerow(["Client's Sample ID", sample.getClientSampleID()])
+            writer.writerow(["Lab Sample ID", sample.id])
+            writer.writerow(["Date Received", date_rec])
+            writer.writerow(["Sampling Date", sampling_date])
+            writer.writerow([])
+            analyses = ar.getAnalyses(full_objects=True)
+            group_cats = {}
+            for analysis in analyses:
+                analysis_info = {'title': analysis.Title(),
+                                 'result': analysis.getFormattedResult(html=False),
+                                 'unit': analysis.getService().getUnit()}
+                if analysis.getCategoryTitle() not in group_cats.keys():
+                    group_cats[analysis.getCategoryTitle()] = []
+                group_cats[analysis.getCategoryTitle()].append(analysis_info)
+
+            for g_cat in sorted(group_cats.keys()):
+                writer.writerow([g_cat])
+                writer.writerow(["Analysis", "Result", "Unit"])
+                for a_info in group_cats[g_cat]:
+                    writer.writerow([a_info['title'], a_info['result'], a_info['unit']])
+
+        return output.getvalue()
