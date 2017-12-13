576249ebeee8ef53d7e23a473199449237c8cdce
Mike Metcalfe <mike@webtide.co.za>
2017-12-13 15:30:45 +0200

    Fixed AR Listing sort of Request ID and Sample ID

--- ace/lims/monkey/monkey_analyses.py
+++ ace/lims/monkey/monkey_analyses.py
@@ -10,11 +10,19 @@ from bika.lims.browser.analysisrequest.analysisrequests import AnalysisRequestsV
 
 def folderitems(self, full_objects=False):
     items = super(AnalysisRequestsView, self).folderitems()
-    #Sort on AR ID sequence number
-    items.sort(lambda x, y: cmp(
-        x['id'].split('-')[3],
-        y['id'].split('-')[3]))
-    ids = [r['id'].split('-')[3] for r in items]
+    if not self.request.get('analysisrequests_sort_on') or \
+       self.request.analysisrequests_sort_on in (
+                'created', 'getRequestID', 'getSample'):
+        #Sort on AR ID sequence number
+        reverse = False
+        if self.request.get('analysisrequests_sort_on') and \
+           self.request.analysisrequests_sort_on != 'created' and \
+           self.request.analysisrequests_sort_order == 'descending':
+            reverse = True
+        items = sorted(
+                items, 
+                lambda x, y: cmp( x['id'].split('-')[3], y['id'].split('-')[3]),
+                reverse=reverse)
     return items
 
 
